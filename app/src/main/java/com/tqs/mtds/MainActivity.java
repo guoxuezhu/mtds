1007cc324456a1938477a181304dadd5d2ee6f6d
gxz <1174327885@qq.com>
2022-08-30 17:45:03 +0800

    fix

--- app/src/main/java/com/tqs/mtds/MainActivity.java
+++ app/src/main/java/com/tqs/mtds/MainActivity.java
@@ -25,7 +25,20 @@ public class MainActivity extends AppCompatActivity {
 
     private void initview() {
 
-        webView.clearHistory();
+
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);//设置js可以直接打开窗口，如window.open()，默认为false
+        webView.getSettings().setJavaScriptEnabled(true);//是否允许执行js，默认为false。设置true时，会提醒可能造成XSS漏洞
+        webView.getSettings().setSupportZoom(true);//是否可以缩放，默认true
+        webView.getSettings().setBuiltInZoomControls(true);//是否显示缩放按钮，默认false
+        webView.getSettings().setUseWideViewPort(true);//设置此属性，可任意比例缩放。大视图模式
+        webView.getSettings().setLoadWithOverviewMode(true);//和setUseWideViewPort(true)一起解决网页自适应问题
+        webView.getSettings().setAppCacheEnabled(true);//是否使用缓存
+        webView.getSettings().setDomStorageEnabled(true);//DOM Storage
+//        webView.getSettings().setUserAgentString("User-Agent:Android");//设置用户代理，一般不用
+
+//        webView.clearHistory();
+
+
         webView.loadUrl("http://rhs.merrytextech.com/business-intelligence/yyksh/bi/view/8c02c9130ceb4a5f0bb2977fd6d6c660");
 
 //        webView.loadUrl("http://mtds.texeasy.net:9180/view/7081d9dc8deb445c10fe20955230729d");
